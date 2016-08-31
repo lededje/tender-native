b5594047dda9d9bd855f3950bb6f11db1eab40a4
Miles Spoor <git@likeminded.io>
2016-08-31 10:15:35 +0100

    Updated icons, launch screen and changed AppDelegate to point at the jsbundle

--- ios/tender/AppDelegate.m
+++ ios/tender/AppDelegate.m
@@ -30,14 +30,8 @@
    * `inet` value under `en0:`) and make sure your computer and iOS device are
    * on the same Wi-Fi network.
    */
-  for (NSString* family in [UIFont familyNames]) {
-    NSLog(@"%@", family);
-    for (NSString* name in [UIFont fontNamesForFamilyName:family]) {
-      NSLog(@" %@", name);
-    }
-  }
 
-//  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+//   jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
